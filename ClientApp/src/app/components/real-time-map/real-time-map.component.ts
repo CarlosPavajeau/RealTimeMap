73fbbcf573619431e9e6909f9212e5a330f4bbdb
Carlos Pavajeau <cantte098@gmail.com>
2021-02-08 19:30:20 -0500

    refactor: Remove code in ngOnInit and refactor to methods

--- ClientApp/src/app/components/real-time-map/real-time-map.component.ts
+++ ClientApp/src/app/components/real-time-map/real-time-map.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { GoogleMap } from "@angular/google-maps";
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { GoogleMap, MapInfoWindow, MapMarker } from "@angular/google-maps";
 import { Observable, of } from "rxjs";
 import { catchError, map } from "rxjs/operators";
 import { environment } from "../../../environments/environment";
@@ -29,23 +29,11 @@ export class RealTimeMapComponent implements OnInit, OnDestroy {
   markerOptions: google.maps.MarkerOptions = {
     icon: {
       url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.hAXyuGkJIUT7hs3isWty5gHaHa%26pid%3DApi&f=1',
-      size: {
-        height: 15,
-        width: 15,
-        equals(other: google.maps.Size): boolean {
-          return false;
-        }
-      },
-      scaledSize: {
-        height: 15,
-        width: 15,
-        equals(other: google.maps.Size): boolean {
-          return false;
-        }
-      }
+      size: new google.maps.Size(15, 15),
+      scaledSize: new google.maps.Size(15, 15),
     },
-    title: 'Hola',
-    label: 'B'
+    title: 'Hello',
+    label: 'Hello there!'
   };
 
   markers: google.maps.Marker[] = [];
@@ -54,6 +42,8 @@ export class RealTimeMapComponent implements OnInit, OnDestroy {
 
   interval;
 
+  @ViewChild(MapInfoWindow) infoWindow: MapInfoWindow;
+
   constructor(private httpClient: HttpClient, private realTimeService: RealTimeMapService) {
   }
 
@@ -61,48 +51,47 @@ export class RealTimeMapComponent implements OnInit, OnDestroy {
     this.realTimeService.startConnection();
     this.realTimeService.addOnUpdateEmployeeLocationListener();
 
-    const id = Math.random() * 20000 + 1000;
-    if (id < 10090) {
-      this.id = '10090';
-    } else {
-      this.id = id.toString();
-    }
+    this.id = (Math.random() * 20000 + 1000).toString();
 
     this.realTimeService.onUpdateEmployeeLocation.subscribe((employeeLocation: EmployeeLocation) => {
-      if (navigator.geolocation) {
-        console.log('New location receive for id ' + employeeLocation.id);
-        console.log(employeeLocation);
-
-        const markerIndex = this.markers.findIndex((marker) => {
-          return marker.getLabel().text == employeeLocation.id;
-        });
-
-        if (markerIndex != -1) {
-          this.markers[markerIndex].setPosition({ lat: employeeLocation.latitude, lng: employeeLocation.longitude });
-        } else {
-          this.markers.push(new google.maps.Marker({
-            position: {
-              lat: employeeLocation.latitude,
-              lng: employeeLocation.longitude
-            },
-            label: {
-              text: employeeLocation.id
-            },
-            title: employeeLocation.id,
-          }));
-        }
-      }
+      this.updateEmployeeLocation(employeeLocation);
     });
 
-    console.log(this.g_map);
-    this.apiLoaded = this.httpClient.jsonp(`https://maps.googleapis.com/maps/api/js?key=${environment.google_maps_api_key}`, 'callback')
-      .pipe(
-        map(() => true),
-        catchError((err) => {
-          console.error(err);
-          return of(false);
-        }),
-      );
+    this.loadGoogleMapsApi();
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.setCurrentPosition);
+    }
+
+    this.interval = setInterval(this.sendMyLocation, 10000);
+  }
+
+  private setCurrentPosition(position: Position) {
+    if (position) {
+      const employeeLocation: EmployeeLocation = {
+        id: this.id,
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      };
+
+      this.realTimeService.sendNewEmployeeLocation(employeeLocation);
+
+      this.options = {
+        center: { lat: position.coords.latitude, lng: position.coords.longitude },
+        zoom: 15
+      };
+      this.markers.push(new google.maps.Marker({
+        position: this.options.center, label: {
+          text: employeeLocation.id,
+        },
+        title: employeeLocation.id
+      }));
+      this.position = { lat: position.coords.latitude + .01, lng: position.coords.longitude - .02 };
+      console.log(position);
+    }
+  }
+
+  private sendMyLocation(): void {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position: Position) => {
         if (position) {
@@ -113,41 +102,53 @@ export class RealTimeMapComponent implements OnInit, OnDestroy {
           };
 
           this.realTimeService.sendNewEmployeeLocation(employeeLocation);
-
-          this.options = {
-            center: { lat: position.coords.latitude, lng: position.coords.longitude },
-            zoom: 15
-          };
-          this.markers.push(new google.maps.Marker({
-            position: this.options.center, label: {
-              text: employeeLocation.id,
-            },
-            title: employeeLocation.id
-          }));
-          this.position = { lat: position.coords.latitude + .01, lng: position.coords.longitude - .02 };
-          console.log(position);
         }
       });
     }
+  }
 
-    this.interval = setInterval(() => {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position: Position) => {
-          if (position) {
-            const employeeLocation: EmployeeLocation = {
-              id: this.id,
-              latitude: position.coords.latitude,
-              longitude: position.coords.longitude
-            };
+  private loadGoogleMapsApi() {
+    this.apiLoaded = this.httpClient.jsonp(`https://maps.googleapis.com/maps/api/js?key=${ environment.google_maps_api_key }`, 'callback')
+      .pipe(
+        map(() => true),
+        catchError((err) => {
+          console.error(err);
+          return of(false);
+        }),
+      );
+  }
 
-            this.realTimeService.sendNewEmployeeLocation(employeeLocation);
-          }
-        });
+  private updateEmployeeLocation(employeeLocation: EmployeeLocation) {
+    if (navigator.geolocation) {
+      console.log('New location receive for id ' + employeeLocation.id);
+      console.log(employeeLocation);
+
+      const markerIndex = this.markers.findIndex((marker) => {
+        return marker.getLabel().text == employeeLocation.id;
+      });
+
+      if (markerIndex != -1) {
+        this.markers[markerIndex].setPosition({ lat: employeeLocation.latitude, lng: employeeLocation.longitude });
+      } else {
+        this.markers.push(new google.maps.Marker({
+          position: {
+            lat: employeeLocation.latitude,
+            lng: employeeLocation.longitude
+          },
+          label: {
+            text: employeeLocation.id
+          },
+          title: employeeLocation.id,
+        }));
       }
-    }, 10000);
+    }
   }
 
   ngOnDestroy() {
     clearInterval(this.interval);
+  }
+
+  openInfoWindow(marker: MapMarker, infoWindow: MapInfoWindow): void {
+    infoWindow.open(marker);
   }
 }
